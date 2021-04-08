d9707926656371ac07101e55826f58bae14644fb
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2021-04-09 00:04:45 +0100

    index.js: modified model index.js

--- server/models/index.js
+++ server/models/index.js
@@ -3,17 +3,13 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
+const dotenv = require('dotenv');
+dotenv.config();
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
-}
+// eslint-disable-next-line prefer-const
+let sequelize = new Sequelize(process.env.DB_CONNECT, config);
 
 fs
   .readdirSync(__dirname)
